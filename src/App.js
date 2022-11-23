1842f0542c1772aacdc575050583f3b0fc5af887
hayaman3 <kevin.haya@yahoo.com>
2022-11-23 21:19:59 +0800

    Refractored Project

--- src/App.js
+++ src/App.js
@@ -1,125 +1,112 @@
-// App.js
+import {useState} from 'react';
+import AddTaskForm from './components/AddTaskForm.jsx';
+import UpdateForm from './components/UpdateForm.jsx';
+import ToDo from './components/ToDo.jsx';
 
-import React, { Component } from "react";
-import uniqid from "uniqid";
-import Overview from "./components/Overview";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      task: {
-        text: '',
-        id: uniqid(),
-        editable: false,
-      },
-      tasks: [],
-      editText: '',
+import './App.css';
 
-      
-    };
+function App() {
+
+  // Tasks (ToDo List) State
+  const [toDo, setToDo] = useState([]);
+
+  // Temp State
+  const [newTask, setNewTask] = useState('');
+  const [updateData, setUpdateData] = useState('');
+
+  // Add task 
+  ///////////////////////////
+  const addTask = () => {
+    if(newTask) {
+      let num = toDo.length + 1; 
+      let newEntry = { id: num, title: newTask, status: false }
+      setToDo([...toDo, newEntry])
+      setNewTask('');
+    }
   }
 
-  handleChange = (e) => {
-    this.setState({
-      task: {
-        text: e.target.value,
-        id: this.state.task.id,
-      },
-    });
-  };
+  // Delete task 
+  ///////////////////////////
+  const deleteTask = (id) => {
+    let newTasks = toDo.filter( task => task.id !== id)
+    setToDo(newTasks);
+  }
 
-  onSubmitTask = (e) => {
-    e.preventDefault();
-    this.setState({
-      tasks: this.state.tasks.concat(this.state.task),
-      task: {
-        text: '', 
-        id: uniqid(),
-      },
-    });
-  };
-
-  deleteTask = (id) => {
-    this.setState({
-      tasks: this.state.tasks.filter(task=>{
-        return task.id !== id
-      })
-    });
-  };
-
-  // editButton = (id,vars) => {
-  //   this.setState({
-  //     tasks: this.state.tasks.filter(task=>{
-  //       if(task.id === id ){
-  //         task.editable = true;
-  //         tasks: this.state.tasks.concat(this.state.task)
-  //       }
-  //       return task 
-  //     })
-      
-  //   });
-  // };
-
-  editButton = (id) => {
-    this.state.tasks.filter(task=>{
-      
-      if(task.id === id ){
-        task.editable = true;
-        // this.setState({
-        //   editText: task.state,
-        // });
-        this.setState((editText) => ({ 
-          editText: task.text,
-       }))
+  // Mark task as done or completed
+  ///////////////////////////
+  const markDone = (id) => {
+    let newTask = toDo.map( task => {
+      if( task.id === id ) {
+        return ({ ...task, status: !task.status })
       }
-      console.log(this.editText)
-      console.log(task.text )
-
-        return task 
-      })
-  };
-
-  saveEditInput = (newtext) => {
-
-        this.setState({
-          editText: this.editText + newtext,
-        });
-  
-  };
-
-  saveEdit  = (e) => {
-    e.preventDefault();
-
-    // this.setState({
-      
-    //     task.text:  e.target.value,
-    // });
-    console.log(e)
-  };
-
-
-  render() {
-    const { task, tasks,} = this.state;
-
-    return (
-      <div>
-        <form onSubmit={this.onSubmitTask}>
-          <label htmlFor="taskInput">Enter task</label>
-          <input
-            onChange={this.handleChange}
-            value={task.text}
-            type="text"
-            id="taskInput"
-          />
-          <button type="submit">
-            Add Task
-          </button>
-        </form>
-        <Overview tasks={tasks} editText={this.editText} deleteTask = {this.deleteTask} editButton = {this.editButton} saveEditInput={this.saveEditInput} saveEdit = {this.saveEdit}/>
-      </div>
-    );
+      return task;
+    })
+    setToDo(newTask);
   }
+
+  // Cancel update
+  ///////////////////////////
+  const cancelUpdate = () => {
+    setUpdateData('');
+  }
+
+  // Change task for update
+  ///////////////////////////
+  const changeTask = (e) => {
+    let newEntry = {
+      id: updateData.id,
+      title: e.target.value,
+      status: updateData.status ? true : false
+    }
+    setUpdateData(newEntry);
+  }
+
+  // Update task
+  ///////////////////////////
+  const updateTask = () => {
+    let filterRecords = [...toDo].filter( task => task.id !== updateData.id );
+    let updatedObject = [...filterRecords, updateData]
+    setToDo(updatedObject);
+    setUpdateData('');
+  }
+
+  return (
+    <div className="container App">
+
+    <br /><br />
+    <h2>To Do List App (ReactJS)</h2>
+    <br /><br />
+
+    {updateData && updateData ? (
+      <UpdateForm 
+        updateData={updateData}
+        changeTask={changeTask}
+        updateTask={updateTask}
+        cancelUpdate={cancelUpdate}
+      />
+    ) : (
+      <AddTaskForm 
+        newTask={newTask}
+        setNewTask={setNewTask}
+        addTask={addTask}
+      />
+    )}
+
+    {/* Display ToDos */}
+
+    {toDo && toDo.length ? '' : 'No Tasks...'}
+
+    <ToDo
+      toDo={toDo}
+      markDone={markDone}
+      setUpdateData={setUpdateData}
+      deleteTask={deleteTask}
+    />  
+
+    </div>
+  );
 }
 
 export default App;
